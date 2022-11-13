af3bec65e02256be822f5be5326d055df5a5dccd
kurehajime <xiidec@gmail.com>
2022-11-13 23:18:37 +0900

    get_node_map

--- src/rule.rs
+++ src/rule.rs
@@ -94,10 +94,13 @@ pub const NUMBERS: [usize; 36] = [
     50, 51, 52, 53, 54, 55,
 ];
 pub type MapArray = Vec<isize>;
+pub type Hand = (usize, usize);
+pub type HandNode = (Hand, MapArray);
 
-pub fn get_can_move_panel_x(panel_num: isize, map: MapArray) -> Vec<usize> {
+pub fn get_can_move_panel_x(panel_num: usize, map: &MapArray) -> Vec<usize> {
     let mut can_move: Vec<usize> = vec![];
-    let number: isize = map[panel_num as usize];
+    let number: isize = map[panel_num];
+    let panel_num = panel_num as isize;
     let x = panel_num / 10;
     let y = panel_num % 10;
     // アガリのコマは動かしたらダメ。何も無いマスも動かしようがない。
@@ -128,4 +131,24 @@ pub fn get_can_move_panel_x(panel_num: isize, map: MapArray) -> Vec<usize> {
         can_move.push(idx);
     }
     can_move
+}
+
+pub fn get_node_map(map: &MapArray, turn_player: isize) -> Vec<HandNode> {
+    let mut node_list: Vec<HandNode> = vec![];
+    for i in 0..36 {
+        let panel_num: usize = NUMBERS[i];
+        if map[panel_num] * turn_player <= 0 || map[panel_num] == 0 {
+            continue;
+        }
+        let can_move = get_can_move_panel_x(panel_num, map);
+        for num in 0..can_move.len() {
+            let mut node_map: MapArray = vec![];
+            node_map[can_move[num]] = node_map[panel_num];
+            node_map[panel_num] = 0;
+            let hand = (panel_num, can_move[num]);
+            let map_array = node_map;
+            node_list.push((hand, map_array));
+        }
+    }
+    return node_list;
 }
--- tests/rule.rs
+++ tests/rule.rs
@@ -21,16 +21,16 @@ fn test_get_can_move_panel_x() {
     map.insert(33, 4);
 
     //1の動きテスト
-    let can_move = get_can_move_panel_x(11, conv_map(&map));
+    let can_move = get_can_move_panel_x(11, &conv_map(&map));
     assert_eq!(can_move, vec![0, 10, 20, 1, 21, 2, 12, 22]);
     //2の動きテスト
-    let can_move = get_can_move_panel_x(31, conv_map(&map));
+    let can_move = get_can_move_panel_x(31, &conv_map(&map));
     assert_eq!(can_move, vec![20, 30, 40, 21, 41, 22, 42]);
     //3の動きテスト
-    let can_move = get_can_move_panel_x(13, conv_map(&map));
+    let can_move = get_can_move_panel_x(13, &conv_map(&map));
     assert_eq!(can_move, vec![2, 12, 22, 4, 14, 24]);
     //4の動きテスト
-    let can_move = get_can_move_panel_x(33, conv_map(&map));
+    let can_move = get_can_move_panel_x(33, &conv_map(&map));
     assert_eq!(can_move, vec![22, 32, 42, 24, 44]);
 
     let mut map = HashMap::new();
@@ -40,16 +40,16 @@ fn test_get_can_move_panel_x() {
     map.insert(33, 8);
 
     //5の動きテスト
-    let can_move = get_can_move_panel_x(11, conv_map(&map));
+    let can_move = get_can_move_panel_x(11, &conv_map(&map));
     assert_eq!(can_move, vec![0, 20, 2, 22]);
     //6の動きテスト
-    let can_move = get_can_move_panel_x(31, conv_map(&map));
+    let can_move = get_can_move_panel_x(31, &conv_map(&map));
     assert_eq!(can_move, vec![20, 40, 32]);
     //7の動きテスト
-    let can_move = get_can_move_panel_x(13, conv_map(&map));
+    let can_move = get_can_move_panel_x(13, &conv_map(&map));
     assert_eq!(can_move, vec![12, 14]);
     //8の動きテスト
-    let can_move = get_can_move_panel_x(33, conv_map(&map));
+    let can_move = get_can_move_panel_x(33, &conv_map(&map));
     assert_eq!(can_move, vec![32]);
 
     let mut map = HashMap::new();
@@ -57,6 +57,6 @@ fn test_get_can_move_panel_x() {
     map.insert(23, 8);
 
     //障害物のテスト
-    let can_move = get_can_move_panel_x(23, conv_map(&map));
+    let can_move = get_can_move_panel_x(23, &conv_map(&map));
     assert_eq!(can_move, vec![]);
 }
