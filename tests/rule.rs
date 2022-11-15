1c302f0012dd9c917152bd6add77f6c5a13e2579
kurehajime <xiidec@gmail.com>
2022-11-15 22:48:43 +0900

    test_think

--- tests/rule.rs
+++ tests/rule.rs
@@ -1,6 +1,10 @@
 use std::collections::HashMap;
 
-use colamone::rule::{get_can_move_panel_x, is_draw, is_end_x, MapArray};
+use colamone::{
+    ai::think_ai,
+    rule::{self, get_can_move_panel_x, is_draw, is_end_x, MapArray},
+};
+use serde_json::Value;
 
 extern crate colamone;
 
@@ -10,6 +14,17 @@ fn conv_map(map: &HashMap<isize, isize>) -> MapArray {
         rtn[*key as usize] = *value as isize;
     }
     return rtn;
+}
+fn json_to_map(json: &str) -> MapArray {
+    let v: Value = serde_json::from_str(json).unwrap();
+    let mut map: MapArray = vec![0; 56];
+    for (key, value) in v.as_object().unwrap().into_iter() {
+        map.insert(
+            key.parse::<usize>().unwrap(),
+            value.as_i64().unwrap() as isize,
+        );
+    }
+    map
 }
 
 #[test]
@@ -104,4 +119,53 @@ fn test_is_end_x() {
     map.insert(15, -6);
     assert_eq!(is_end_x(&conv_map(&map), false), 0);
     assert_eq!(is_draw(&conv_map(&map)), true);
+}
+
+#[test]
+fn test_think() {
+    let mut map = json_to_map(
+        "{
+        0: -1, 10: -2, 20: -3, 30: -4, 40: -5, 50: -6,
+        1: 0, 11: -8, 21: 0, 31: 0, 41: -7, 51: 0,
+        2: 0, 12: 0, 22: 0, 32: 0, 42: 0, 52: 0,
+        3: 0, 13: 0, 23: 0, 33: 0, 43: 0, 53: 0,
+        4: 0, 14: 7, 24: 0, 34: 0, 44: 8, 54: 0,
+        5: 6, 15: 5, 25: 4, 35: 3, 45: 2, 55: 1,
+    }",
+    );
+    let mut turn_player = 1;
+    let mut end;
+    let mut count = 0;
+    let result = json_to_map(
+        "
+        {
+            0: 5, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0,
+            10: 6, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0,
+            20: 0, 21: 0, 22: 0, 23: 0, 24: 0, 25: 0,
+            30: 0, 31: 0, 32: 0, 33: 0, 34: 0, 35: 0,
+            40: 0, 41: 0, 42: 0, 43: 0, 44: 0, 45: -2,
+            50: -6, 51: 0, 52: 0, 53: 0, 54: 0, 55: 1
+        }",
+    );
+    while count <= 255 {
+        count += 1;
+        if count > 255 {
+            break;
+        }
+        let hand = think_ai(&map, turn_player, 3, None, None, None).0;
+        if hand != None {
+            map[hand.unwrap().1] = map[hand.unwrap().0];
+            map[hand.unwrap().0] = 0;
+        }
+        if rule::is_draw(&map) == true {
+            break;
+        }
+        end = rule::is_end_x(&map, false);
+        if end == 1 || end == -1 {
+            break;
+        }
+        turn_player = turn_player * -1
+    }
+
+    assert_eq!(map, result);
 }
